44cd96188bfb9c885be3ec27d3498460e7c50d3f
A-lineed <alineedvania2018@gmail.com>
2021-09-09 17:18:12 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsvendas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,19 +11,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsvendas.dto.SaleDTO;
+import com.devsuperior.dsvendas.dto.SaleSucessDTO;
+import com.devsuperior.dsvendas.dto.SaleSumDTO;
 import com.devsuperior.dsvendas.service.SaleService;
 
-@RestController //Controlador rest.
+@RestController // Controlador rest.
 @RequestMapping(value = "/sales")
 public class SaleController {
-	
+
 	@Autowired
 	private SaleService service;
-	
+
 	@GetMapping
 	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable) {
 		Page<SaleDTO> list = service.findAll(pageable);
 		return ResponseEntity.ok(list);
-		}
+	}
+
+	@GetMapping(value = "/amount-by-seller") // Soma da venda por vendedor
+	public ResponseEntity<List<SaleSumDTO>> amountGroupeBySeller() {
+		List<SaleSumDTO> list = service.amountGroupeBySeller();
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/sucess-by-seller") // Soma da venda por vendedor
+	public ResponseEntity<List<SaleSucessDTO>> sucessGroupeBySeller() {
+		List<SaleSucessDTO> list = service.sucessGroupeBySeller();
+		return ResponseEntity.ok(list);
+	}
 
 }
