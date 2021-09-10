65aecc383779acf4496bd94c8a1c13e4ff38c6b3
A-lineed <alineedvania2018@gmail.com>
2021-09-10 20:51:53 -0300

    Finalizando

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -23,7 +23,7 @@ function Dashboard() {
   
           <div className="py-3">
   
-            <h2 className="text-primary">Todas vendasx </h2>
+            <h2 className="text-primary">Todas vendas </h2>
           </div>
   
           </div>
