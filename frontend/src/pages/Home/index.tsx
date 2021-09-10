652a107eb11b06eaa1139859baac68d030450cda
A-lineed <alineedvania2018@gmail.com>
2021-09-10 14:31:07 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,12 +1,26 @@
-function Home() {
-    return (
-      <div>
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
 
-          <h1>Página Home</h1>
+function Home() {
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">DSVendas</h1>
+          <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+          <hr />
+          <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+          <Link className="btn btn-primary btn-lg" to="/dashboard">
+            Acessar dashboard
+          </Link>
+
         </div>
-     
-    );
-  }
-  
-  export default Home;
-  
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
